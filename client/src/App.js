87a4b6663bf7bebd5601240653e666faad29ca5d
Rutenis Turcinas <rutenis.turcinas@gmail.com>
2017-02-02 17:14:37 +0200

    Code formatting.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {data: null};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {data: null};
+    }
 
-  handleClick = () => {
+    handleClick = () => {
         fetch('/getdata')
             .then((response) => {
                 return response.json();
@@ -21,19 +21,20 @@ class App extends Component {
             });
     };
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2 onClick={this.handleClick}>{this.state.data ? this.state.data : "Welcome to React"}</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2 onClick={this.handleClick}>{this.state.data ? this.state.data : "Welcome to React"}</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+
 }
 
 export default App;
