4839a26933e456258907c4adcd663c76b8aefa74
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2022-06-16 01:01:10 +0300

    Update TS compiler and some styles

--- client/src/routes/join-quiz/JoinQuiz.js
+++ client/src/routes/join-quiz/JoinQuiz.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {browserHistory} from "react-router";
+import React, { Component } from "react";
+import { browserHistory } from "react-router";
 import ParticipantEntity from "../../model/entities/ParticipantEntity";
 import Logo from "../../components/logo/Logo";
 import Form from "../../components/form/Form";
@@ -9,43 +9,64 @@ import Utils from "../../utils/Utils";
 import "./JoinQuiz.css";
 
 export default class JoinQuiz extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            participant: new ParticipantEntity(),
-            errors: {}
-        };
-    }
-
-    handleChange = (event) => {
-        let participant = Utils.handleChange(event, this.state.participant);
-        this.setState({participant: participant, errors: {}});
+    this.state = {
+      participant: new ParticipantEntity(),
+      errors: {},
     };
+  }
 
-    handleJoin = (event) => {
-        event.preventDefault();
+  handleChange = (event) => {
+    let participant = Utils.handleChange(event, this.state.participant);
+    this.setState({ participant: participant, errors: {} });
+  };
 
-        delete this.state.participant.answeredQuestionIds;
+  handleJoin = (event) => {
+    event.preventDefault();
 
-        Utils.ajaxPost(Constants.REST_API_PREFIX + '/participant/join', this.state.participant).then((result) => {
-            browserHistory.push('/quiz/' + result._id);
-        }).catch((err) => this.setState({errors: err}));
-    };
+    delete this.state.participant.answeredQuestionIds;
 
-    render() {
-        return (
+    Utils.ajaxPost(
+      Constants.REST_API_PREFIX + "/participant/join",
+      this.state.participant
+    )
+      .then((result) => {
+        browserHistory.push("/quiz/" + result._id);
+      })
+      .catch((err) => this.setState({ errors: err }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Logo bottomSpacing={true} />
+        <Form onSubmit={this.handleJoin} submitText="Join Quiz">
+          <div className="JoinQuiz">
             <div>
-                <Logo bottomSpacing={true}/>
-                <Form onSubmit={this.handleJoin} submitText="Join Quiz">
-                    <div className="JoinQuiz">
-                        <Input label="YOUR NAME" error={this.state.errors['participantName']} floatLeft={true} placeholder="Name or Nickname" onChange={this.handleChange} name="participantName" value={this.state.participant.participantName}/>
-                        <Input label="QUIZ CODE" error={this.state.errors['quizCode']} floatRight={true} placeholder="Quiz Code" onChange={this.handleChange} name="quizCode" value={this.state.participant.quizCode}/>
-                    </div>
-                </Form>
+              <Input
+                label="YOUR NAME"
+                error={this.state.errors["participantName"]}
+                floatLeft={true}
+                placeholder="Name or Nickname"
+                onChange={this.handleChange}
+                name="participantName"
+                value={this.state.participant.participantName}
+              />
             </div>
-        );
-    }
-
+            <Input
+              label="QUIZ CODE"
+              error={this.state.errors["quizCode"]}
+              floatRight={true}
+              placeholder="Quiz Code"
+              onChange={this.handleChange}
+              name="quizCode"
+              value={this.state.participant.quizCode}
+            />
+          </div>
+        </Form>
+      </div>
+    );
+  }
 }
